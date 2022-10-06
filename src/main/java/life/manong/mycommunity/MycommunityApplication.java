d5311f5cccc851dea2e96f04df34567809d78525
Myuan <1209619967@qq.com>
2022-10-06 15:37:29 +0800

    one commit

---
+++ src/main/java/life/manong/mycommunity/MycommunityApplication.java
@@ -0,0 +1,17 @@
+package life.manong.mycommunity;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycommunityApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MycommunityApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/life/manong/mycommunity/MycommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package life.manong.mycommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
