fba36d5f6943aee1877da0fdeab62337aba2b940
Myuan <1209619967@qq.com>
2022-10-08 08:20:33 +0800

    five commit

--- src/main/java/life/manong/mycommunity/controller/AuthorizeController.java
+++ src/main/java/life/manong/mycommunity/controller/AuthorizeController.java
@@ -3,6 +3,8 @@ package life.manong.mycommunity.controller;
 import life.manong.mycommunity.dto.AccessTokenDTO;
 
 import life.manong.mycommunity.dto.GithubUser;
+import life.manong.mycommunity.mapper.UserMapper;
+import life.manong.mycommunity.model.User;
 import life.manong.mycommunity.provider.GithubProvider;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 @Slf4j
@@ -27,6 +32,9 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
+
     /**
      * 调用方法，返回access_token
      * @param code
@@ -35,7 +43,8 @@ public class AuthorizeController {
      */
     @GetMapping("/callback")
     public String callback(@RequestParam("code") String code,
-                           @RequestParam("state") String state
+                           @RequestParam("state") String state,
+                           HttpServletRequest request
                            ){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
@@ -44,9 +53,35 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        log.info(user.toString());
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if (githubUser != null){
+            User user = new User();
+            user.setAccountId(githubUser.getId().toString());
+            user.setToken(UUID.randomUUID().toString());
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            user.setName(githubUser.getName());
+//            userMapper.insert(user);
+            request.getSession().setAttribute("user",githubUser);
+        }else {
+            return "redirect:/";
+        }
 
         return "index";
+    }
+
+    @GetMapping("/insert")
+    public String testInsert(){
+
+        User user = new User();
+
+        user.setName("马远");
+        user.setAccountId("123245");
+        user.setToken("1241235245");
+        user.setGmtModified(System.currentTimeMillis());
+        user.setGmtCreate(System.currentTimeMillis());
+        userMapper.insert(user);
+
+        return "success";
     }
 }
--- src/test/java/life/manong/mycommunity/MycommunityApplicationTests.java
+++ src/test/java/life/manong/mycommunity/MycommunityApplicationTests.java
@@ -6,8 +6,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MycommunityApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
