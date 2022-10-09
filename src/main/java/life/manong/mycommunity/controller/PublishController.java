42536a87686b778f81ecd340a08c1d0a70c7150a
Myuan <1209619967@qq.com>
2022-10-09 11:07:49 +0800

    seven commit

--- src/main/java/life/manong/mycommunity/controller/PublishController.java
+++ src/main/java/life/manong/mycommunity/controller/PublishController.java
@@ -4,6 +4,8 @@ import life.manong.mycommunity.mapper.QuestionMapper;
 import life.manong.mycommunity.model.Question;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,6 +20,7 @@ public class PublishController {
 
     @GetMapping("/publish")
     public String publish(){
+
         return "publish";
     }
 
@@ -26,13 +29,26 @@ public class PublishController {
             @RequestParam("title") String title,
             @RequestParam("description") String description,
             @RequestParam("tag") String tag,
-            HttpServletRequest request
+            HttpServletRequest request,
+            Model model
+
     ){
+        //将数据设置到域中，便于回显
+        model.addAttribute("title",title);
+        model.addAttribute("description",description);
+        model.addAttribute("tag",tag);
+        /**
+         * 进行验证如果已经登陆，则可以进行发布
+         * 但是我的账号被封禁，所以暂时无法实现
+         */
+
         Question question = new Question();
         question.setTitle(title);
         question.setDescription(description);
-
         question.setTag(tag);
+        question.setGmtCreate(System.currentTimeMillis());
+        question.setGmtModified(System.currentTimeMillis());
+        question.setCreator(123456);
 
         questionMapper.create(question);
 
--- src/main/java/life/manong/mycommunity/mapper/QuestionMapper.java
+++ src/main/java/life/manong/mycommunity/mapper/QuestionMapper.java
@@ -8,6 +8,6 @@ import org.apache.ibatis.annotations.Mapper;
 public interface QuestionMapper {
 
 
-    @Insert("insert into question (title,description,gmt_create,gmt_modified,creator,tag) values (#{title},#{description},#{gmt_create},#{gmt_modified},#{creator,tag})")
+    @Insert("insert into question (title,description,gmt_create,gmt_modified,creator,tag) values (#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tag})")
     void create(Question question);
 }
